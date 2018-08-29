ede238a6d07de4c54abb9f85131becdd1fcc3131
alexei <alexeidequesada@gmail.com>
2018-08-29 18:57:28 -0300

    fixing refresh table after deleting row

--- src/app/Grupo/add-grupo/add-grupo.component.ts
+++ src/app/Grupo/add-grupo/add-grupo.component.ts
@@ -1,23 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-import {Router} from "@angular/router";
+import { Router } from '@angular/router';
 import { GrupoService } from '../../Service/grupo.service';
 import { Grupo } from '../../Interface/grupo';
 
 @Component({
   selector: 'app-add-grupo',
   templateUrl: './add-grupo.component.html',
-  providers: [ GrupoService ],
+  providers: [GrupoService],
   styleUrls: ['./add-grupo.component.css']
 })
-export class AddGrupoComponent implements OnInit { 
-  grupo: Grupo; 
+export class AddGrupoComponent implements OnInit {
+  grupo: Grupo;
 
-  constructor(private router: Router, private grupoService: GrupoService) { 
+  constructor(private router: Router, private grupoService: GrupoService) {
   }
 
   ngOnInit() {
   }
- 
+
   /*Take data sended from view and send it to the service */
   criarGrupo(nome: string): void {
     nome = nome.trim();
@@ -29,6 +29,6 @@ export class AddGrupoComponent implements OnInit {
       this.router.navigate(['/grupos']);
     });
 
-  } 
+  }
 
 }
