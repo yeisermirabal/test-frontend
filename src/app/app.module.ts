01a05763d0291951f81f968092916fc8146d7b5c
alexei <alexeidequesada@gmail.com>
2018-09-02 00:45:27 -0300

    fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,33 @@
+// dependencies
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 import { LayoutModule } from '@angular/cdk/layout';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { AppComponent } from './app.component';
 import { TextMaskModule } from 'angular2-text-mask';
 
-/*Components related to Clients*/
+// components
+import { AppComponent } from './app.component';
 import { AddClienteComponent } from './Components/Cliente/add-cliente/add-cliente.component';
 import { EditClienteComponent } from './Components/Cliente/edit-cliente/edit-cliente.component';
 import { ListClienteComponent } from './Components/Cliente/list-cliente/list-cliente.component';
-/*Components related to Groups*/
 import { AddGrupoComponent } from './Components/Grupo/add-grupo/add-grupo.component';
 import { EditGrupoComponent } from './Components/Grupo/edit-grupo/edit-grupo.component';
 import { ListGrupoComponent } from './Components/Grupo/list-grupo/list-grupo.component';
-/*File with routes*/
-import { routing } from './routing';
-import { MaterialModule } from './Modules/material';
-/*Import Service*/
+import { NavigationComponent } from './Components/Navigation/navigation.component';
+import { DashboardComponent } from './Components/Dashboard/dashboard.component';
+
+// services
 import { ClienteService } from './Services/Cliente/cliente.service';
 import { GrupoService } from './Services/Grupo/grupo.service';
 import { ViaCepService } from './Services/ViaCep/via-cep.service';
 
-import { NavigationComponent } from './Components/Navigation/navigation.component';
-import { DashboardComponent } from './Components/Dashboard/dashboard.component';
+// routing
+import { routing } from './routing';
+
+// UI
+import { MaterialModule } from './Modules/material';
 
 @NgModule({
   declarations: [
@@ -47,7 +50,8 @@ import { DashboardComponent } from './Components/Dashboard/dashboard.component';
     HttpClientModule,
     LayoutModule,
     FormsModule,
-    TextMaskModule
+    TextMaskModule,
+    ModalDialogModule.forRoot()
   ],
   providers: [ClienteService, GrupoService, ViaCepService],
   bootstrap: [AppComponent]
