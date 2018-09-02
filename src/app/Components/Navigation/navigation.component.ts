d715ca3a00a0941dd3fd8c41f17bde19bd3625a0
alexei <alexeidequesada@gmail.com>
2018-09-01 22:05:20 -0300

    fixes

--- src/app/Components/Navigation/navigation.component.ts
+++ src/app/Components/Navigation/navigation.component.ts
@@ -11,10 +11,7 @@ import { map } from 'rxjs/operators';
 export class NavigationComponent {
 
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
-    .pipe(
-      map(result => result.matches)
-    );
+    .pipe(map(result => result.matches));
 
   constructor(private breakpointObserver: BreakpointObserver) { }
-
 }
